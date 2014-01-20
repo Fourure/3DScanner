947af7f531374153490b9466cfc99bc46ffd952d
Lelene <perrier.ln@gmail.com>
2014-01-20 12:00:44 +0100

    Matching works as hell
    But ruins performances ><
    TODO : get trafo

--- src/openni_grabber.cpp
+++ src/openni_grabber.cpp
@@ -5,64 +5,104 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
+#define XTION_CLOUD
 
 class SimpleOpenNIViewer
 {
 	public:
-		SimpleOpenNIViewer ()  : viewer("Point Cloud") {}
-		
-		cv::Mat convertToCvMat(const openni_wrapper::DepthImage::ConstPtr dim)
+		SimpleOpenNIViewer ()  : viewer("Point Cloud"), rgb_(480, 640, CV_8UC3), depth_(480, 640, CV_32FC1), tmp_rgb_(480, 640, CV_8UC3) 
 		{
-			cv::Mat frameDepth(dim->getHeight(),dim->getWidth(), CV_32FC1);
-			
-			dim->fillDepthImage(frameDepth.cols,frameDepth.rows, (float*)frameDepth.data,frameDepth.step);
-			
-			return frameDepth;
+			frame_point_cloud_ = new pcl::PointCloud<pcl::PointXYZRGBA>;
 		}
 		
-		cv::Mat convertToCvMat(const openni_wrapper::Image::ConstPtr im)
+		void convertToCvMat(const openni_wrapper::DepthImage::ConstPtr dim, cv::Mat& frameDepth)
 		{
-			cv::Mat frameRGB(im->getHeight(),im->getWidth(), CV_8UC3);
-			
+			dim->fillDepthImage(frameDepth.cols,frameDepth.rows, (float*)frameDepth.data,frameDepth.step);
+		}
+		
+		void convertToCvMat(const openni_wrapper::Image::ConstPtr im, cv::Mat& frameRGB)
+		{
 			im->fillRGB(frameRGB.cols,frameRGB.rows,frameRGB.data,frameRGB.step);
-			
-			return frameRGB;
 		}
 
+		//Gets transform between clouds and call for reconstruction
 		void camerasCallback (	const boost::shared_ptr<openni_wrapper::Image>& im, 
 							const boost::shared_ptr<openni_wrapper::DepthImage>& dim, 
 							float constant	  )
 		{
-			cv::Mat depth_image_ = convertToCvMat(dim);
-			cv::Mat rgb_image_ = convertToCvMat(im);
+			tmp_rgb_ = rgb_;
 			
-			cv::cvtColor(rgb_image_, rgb_image_, CV_RGB2BGR );
+			convertToCvMat(dim, depth_);
+			convertToCvMat(im, rgb_);
+			cv::cvtColor(rgb_, rgb_, CV_RGB2BGR);
 			
-			cv::imshow("RGB", rgb_image_);
-			cv::imshow("Depth", depth_image_);
+			computeTransformation();
+		}
+		
+		void computeTransformation()
+		{
+			cv::SiftFeatureDetector detector( 0.05, 5.0 );
+			cv::SiftDescriptorExtractor extractor( 3.0 );
+			
+			cv::Mat im1 = tmp_rgb_;
+			cv::Mat im2 = rgb_;
+			
+			std::vector<cv::KeyPoint> key1, key2;
+			detector.detect( im1, key1 );
+			detector.detect( im2, key2 );
+			
+			drawKeypoints(im1,key1,rgb_,cv::Scalar(255, 0, 0));
+			
+			cv::Mat desc1, desc2;
+			extractor.compute( im1, key1, desc1 );
+			extractor.compute( im2, key2, desc2 );
+			
+			cv::FlannBasedMatcher matcher;
+			std::vector< cv::DMatch > matches;
+			matcher.match( desc1, desc2, matches );
+			
+			cv::Mat result;
+			drawMatches( im1, key1, im2, key2, matches, result );
+			
+			//cv::imwrite("toto.png", result); Works as hell =D !
+			
+			show();
+		}
+		
+		//Display Flows
+		void show()
+		{
+			cv::imshow("RGB", rgb_);
+			cv::imshow("Depth", depth_);
 			cv::waitKey(1);
 		}
 		
+		//called if the XTION point cloud is used
 		void cloudCallback (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr &cloud)
 		{
-			if (!viewer.wasStopped())
-				viewer.showCloud (cloud);
+			viewer.showCloud(cloud); //to move in show but f****** boost and its denominations prevent me from doing it
+			delete frame_point_cloud_;
+			frame_point_cloud_ = new pcl::PointCloud<pcl::PointXYZRGBA>(*cloud);
+			show();
 		}
 
 		void run ()
 		{
 			pcl::Grabber* interface = new pcl::OpenNIGrabber();
 			
-			boost::function<void (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr&)> c_cb =
-				boost::bind (&SimpleOpenNIViewer::cloudCallback, this, _1);
-			
 			boost::function<void (const boost::shared_ptr<openni_wrapper::Image>&, 
 				const boost::shared_ptr<openni_wrapper::DepthImage>&, 
 				float constant)>  nc_cb = boost::bind (&SimpleOpenNIViewer::camerasCallback, this, _1, _2, _3);
-			
 			interface->registerCallback (nc_cb);
+			
+			#ifdef XTION_CLOUD
+			boost::function<void (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr&)> c_cb =
+				boost::bind (&SimpleOpenNIViewer::cloudCallback, this, _1);
 			interface->registerCallback (c_cb);
-		     
+			#endif
+			
 			interface->start();
 
 			while (!viewer.wasStopped())
@@ -71,7 +111,13 @@ class SimpleOpenNIViewer
 			interface->stop();
 		}
 		
+		//cloud viewer, pcl facilities
 		pcl::visualization::CloudViewer viewer;
+		cv::Mat rgb_;
+		cv::Mat depth_;
+		pcl::PointCloud<pcl::PointXYZRGBA>* frame_point_cloud_;
+		
+		cv::Mat tmp_rgb_; //last frame, used for SIFT transformation computing
 };
 
 int main ()
